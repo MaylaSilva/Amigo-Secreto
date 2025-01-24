ac0a3842180173972db801adbe70de0aca611312
MaylaSilva <may.siqueira93@gmail.com>
2025-01-24 12:35:03 -0300

    Feat: Incluir função adicionarAmigo
    Inclusa a função de adicionar amigo a lista

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,52 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let nomeLista;
+
+function adicionarAmigo() {
+    adicionar = document.querySelector('input').value;
+    if (adicionar == ''){
+        return alert('Campo em branco. Por favor, digite um nome antes de adicionar o amigo');
+    }
+    if (amigos.includes(adicionar)) {
+        return alert('O nome já foi incluído. Altere o nome ou inclua o sobrenome');
+    } else {
+        amigos.push(adicionar);
+        limparCampo();
+        listarAmigos();
+    }
+}
+
+function sortearAmigo() {
+    let numeroAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[numeroAleatorio];
+
+    if (amigos.length <= 1){
+        return alert ('Precisa incluir os amigos antes de sortear');
+    }
+    
+    let exibirResultado = document.getElementById('resultado');
+    exibirResultado.innerHTML = `<li> O amigo sorteado é: ${amigoSorteado}</li>`;
+}
+
+function limparCampo() {
+    let limpar = document.querySelector('input');
+    limpar.value = '';
+}
+
+function listarAmigos() {
+    let exibirLista = document.getElementById('listaAmigos');
+    exibirLista.innerHTML = ''; //Limpando a lista antes de iterar
+
+    amigos.forEach(amigo => {
+        let li = document.createElement('li');
+        li.textContent = amigo; //Aqui é onde é incluso o nome na lista do HTML
+        exibirLista.appendChild(li);
+    });
+} 
+
+function limparLista() {
+
+}
+
+function exibirTexto() {
+
+}
